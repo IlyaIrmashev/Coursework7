e69ffe378cf270f90cad72a2e463e1d5415507d2
Ilya Irmashev <ilya290397@gmail.com>
2024-02-14 22:20:04 +0400

    Cw7

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from users.models import User
 from users.permissions import IsModerator, IsOwner
